9af4c98d09eb7f3f61bdba64bfab035b2d733d94
natanferreirah <natanferreiradon31@gmail.com>
2025-09-14 11:57:36 -0300

    Design para Listar Feito

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,14 +1,26 @@
-<table>
-    <th>Nome</th>
-    <th>Preço</th>
-    <th>Estoque</th>
-@foreach($products as $product)
-    <tr>
-        <td>{{ $product->name }}</td>
-        <td>{{ $product->price }}</td>
-        <td>{{ $product->stock }}</td>
-        <td><a href="{{ route('products.edit', $product->id)}}">Editar</a></td>
-        <td> <a href="{{ route('products.destroy', $product->id)}}">Excluir</a></td>
-    </tr>
-    @endforeach
-</table>    
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <title>StockOnline</title>
+</head>
+<body>
+    <table>
+        <th>Nome</th>
+        <th>Preço</th>
+        <th>Estoque</th>
+        <th><a href="{{ route('products.create') }}" class="content_button"><button class="create_button">Cadastrar Produto</button></a>
+        @foreach($products as $product)</th>
+            <tr>
+                <td class="content"> {{ $product->name }}</td>
+                <td class="content"> {{ $product->price }}</td>
+                <td class="content"> {{ $product->stock }}</td>
+                <td class="button"><a href="{{ route('products.edit', $product->id)}}">Editar</a></td>
+                <td class="button"> <a href="{{ route('products.destroy', $product->id)}}">Excluir</a></td>
+            </tr>
+        @endforeach
+    </table>    
+</body>
+</html>
