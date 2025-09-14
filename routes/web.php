baba584d36061dbf2ada5ec0946f2cde2756343e
natanferreirah <natanferreiradon31@gmail.com>
2025-09-14 00:46:02 -0300

    Erro de síntaxe corrigido

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
 
 
-Route::get('/products', ['ProductController::class', 'index'])->name('products.index');
-Route::get('/products/create', ['ProductController::class', 'create'])->name('products.create');
-Route::post('/products', ['ProductController::class', 'store'])->name('products.store');
-Route::post('/products/{product}', ['ProductController::class', 'show'])->name('products.show');
-Route::post('/products{product}/edit', ['ProductController::class', 'edit'])->name('products.edit');
-Route::put('/products/{product}', ['ProductController::class', 'update'])->name('products.update');
-Route::delete('/products/{product}', ['ProductController::class', 'destroy'])->name('products.destroy');
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+Route::post('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+Route::post('/products{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
  
