c1892794e27c7e0569c1fffc297222c10fadb9d6
natanferreirah <natanferreiradon31@gmail.com>
2025-09-19 23:30:15 -0300

    Alteração e correção do código

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,5 @@ Route::post('/products', [ProductController::class, 'store'])->name('products.st
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
 Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
 Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
-Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
  
